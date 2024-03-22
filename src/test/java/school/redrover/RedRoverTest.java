4e8deeea8dbeaf8f7953eb9c6e74b1e2a467a2bf
Sergey.D <sergei.demyanenko@gmail.com>
2024-03-21 21:08:27 -0400

    Add first test

---
+++ src/test/java/school/redrover/RedRoverTest.java
@@ -0,0 +1,37 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class RedRoverTest {
+
+    @Test
+    public void testTest() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+
+        WebElement textBox = driver.findElement( By.name("my-text") );
+        textBox.sendKeys("Selenium");
+
+        WebElement textArea = driver.findElement(By.name("my-textarea"));
+        textArea.sendKeys("Sergey!");
+
+        WebElement submitButton = driver.findElement(By.className("btn"));
+        submitButton.click();
+
+        WebElement message = driver.findElement(By.id("message"));
+        String value = message.getText();
+
+        Assert.assertEquals(value, "Received!");
+
+        driver.quit();
+    }
+}
