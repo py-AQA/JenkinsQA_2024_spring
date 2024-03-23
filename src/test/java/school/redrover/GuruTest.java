10ad9344c6649fb97db1ecb551c22cd846e6e1c0
Valentin Podkova <val.podkova@gmail.com>
2024-03-23 17:48:26 +0200

    add my new test

---
+++ src/test/java/school/redrover/GuruTest.java
@@ -0,0 +1,33 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+import java.time.Duration;
+
+public class GuruTest {
+    @Test
+    public void FormTest() {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+
+        driver.get("https://demo.guru99.com/test/login.html");
+
+        WebElement email = driver.findElement(By.id("email"));
+        email.sendKeys("abcd@gmail.com");
+
+        WebElement password = driver.findElement(By.name("passwd"));
+        password.sendKeys("abcdefghlkjl");
+
+        WebElement login = driver.findElement(By.id("SubmitLogin"));
+        email.sendKeys("abcd@gmail.com");
+        password.sendKeys("abcdefghlkjl");
+        login.click();
+
+        driver.quit();
+
+    }
+}
