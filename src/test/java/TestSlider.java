fe3a494822cd0e95453d3194ededcdc2991c6c3f
Viktoriia <yvika2909@gmail.com>
2024-03-24 16:08:51 +0200

    Add new test

---
+++ src/test/java/TestSlider.java
@@ -0,0 +1,47 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestSlider {
+    @Test
+    public void TestSlider() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/horizontal_slider");
+
+
+        WebElement slider = driver.findElement(By.xpath("/html/body/div[2]/div/div/div/input"));
+        WebElement text = driver.findElement(By.xpath("//*[@id='range']"));
+
+
+        Actions actions = new Actions(driver);
+
+        double expectedValue = 0;
+        Assert.assertEquals(Double.parseDouble(text.getText()), expectedValue);
+        final double step = 0.5;
+        for (int i = 0; i < 10; i++) {
+            slider.sendKeys(Keys.ARROW_RIGHT);
+            expectedValue += step;
+            Assert.assertEquals(Double.parseDouble(text.getText()), expectedValue);
+        }
+        for (int i = 0; i < 11; i++) {
+            Assert.assertEquals(Double.parseDouble(text.getText()), expectedValue);
+            slider.sendKeys(Keys.ARROW_LEFT);
+            expectedValue -= step;
+        }
+
+        String sliderValue = slider.getAttribute("value");
+        System.out.println("Slider Value: " + sliderValue);
+        System.out.println("Slider min " + slider.getAttribute("min"));
+        System.out.println("Slider max " + slider.getAttribute("max"));
+
+        driver.quit();
+    }
+
+}
+
+
