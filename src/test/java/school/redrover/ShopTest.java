c49a2934a040002275fbd15df6421bd9fa402ca1
ZalinaZN <zaseeva1985@mail.ru>
2024-03-24 16:27:15 +0300

    refactoring code ShopTest

--- src/test/java/school/redrover/ShopTest.java
+++ src/test/java/school/redrover/ShopTest.java
@@ -11,14 +11,14 @@ import org.testng.annotations.Test;
 
 public class ShopTest {
     @Test
-    public void basketTest() throws InterruptedException {
+    public void testCart() throws InterruptedException {
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.setPageLoadStrategy(PageLoadStrategy.EAGER);
         WebDriver driver = new ChromeDriver(chromeOptions);
         driver.get("https://magento.softwaretestingboard.com/");
         driver.manage().window().maximize();
 
-        Thread.sleep(5000);
+        Thread.sleep(3000);
         WebElement salePageButton = driver.findElement(By.id("ui-id-8"));
         salePageButton.click();
         WebElement shopWomanDealButton = driver.findElement(By.xpath("//span[@class = 'more button']"));
@@ -26,19 +26,19 @@ public class ShopTest {
         WebElement bessYogaShortItemLink = driver.
                 findElement(By.xpath("//a[contains(., 'Bess')]"));
         bessYogaShortItemLink.click();
-        Thread.sleep(5000);
+        Thread.sleep(2000);
         WebElement sizeSelect = driver.findElement(By.xpath("//div[@option-id='171']"));
         sizeSelect.click();
-        Thread.sleep(5000);
+        Thread.sleep(2000);
         WebElement colorSelect = driver.findElement(By.xpath("//div[@option-id='50']"));
         colorSelect.click();
         WebElement addToCard = driver.findElement(By.id("product-addtocart-button"));
         addToCard.click();
-        Thread.sleep(5000);
+        Thread.sleep(3000);
         WebElement cartCounterNumber = driver.findElement(By.xpath("//span[@class ='counter-number']"));
         cartCounterNumber.click();
 
-        Thread.sleep(5000);
+        Thread.sleep(3000);
         Assert.assertEquals((driver.findElement(By
                         .xpath("//strong[@class='product-item-name']/a")).getText()),
                 "Bess Yoga Short");
