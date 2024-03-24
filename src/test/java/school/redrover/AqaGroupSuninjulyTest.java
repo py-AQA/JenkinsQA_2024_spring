df396989cfa6ab92e400bd8ad2b1daefebab9f6e
aqaestuser <144237758+aqaestuser@users.noreply.github.com>
2024-03-24 14:02:54 +0300

    [+] using findElements

---
+++ src/test/java/school/redrover/AqaGroupSuninjulyTest.java
@@ -0,0 +1,41 @@
+package school.redrover;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.List;
+
+public class AqaGroupSuninjulyTest {
+    @Test
+    public void testHugeForm() {
+        String link = "https://suninjuly.github.io/huge_form.html";
+        WebDriver driver = new ChromeDriver();
+
+        try {
+            driver.get(link);
+
+            List<WebElement> elements = driver.findElements(By.tagName("input"));
+            for (WebElement item : elements) {
+                item.sendKeys("Мой ответ");
+            }
+
+            WebElement button = driver.findElement(By.cssSelector("button.btn"));
+            button.click();
+
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+            Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+
+            Assert.assertTrue(alert.getText().startsWith("Congrats, you've passed the task!"), "wrong answer");
+        } finally {
+            driver.quit();
+        }
+    }
+}
