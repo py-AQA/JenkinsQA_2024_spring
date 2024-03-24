8b7565fa53e8d2455987e90891d8e66cde605613
Serghei M. <mex.ser@yandex.ru>
2024-03-25 00:59:19 +0200

    update my  test

--- src/test/java/school/redrover/WordHuntTest.java
+++ src/test/java/school/redrover/WordHuntTest.java
@@ -26,6 +26,7 @@ public class WordHuntTest {
         WebElement translation = driver.findElement(By.className("t_inline_en"));
         String value = translation.getText();
 
+
         Assert.assertEquals("яблоко, яблоня, чепуха, лесть, яблочный", value);
         driver.quit();
 
