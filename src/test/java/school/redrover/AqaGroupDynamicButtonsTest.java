40ec8e11eda10773709b84f034bf914f7f15c761
iKuper <160205258+KuperWoman@users.noreply.github.com>
2024-03-24 11:58:14 +0300

    iKuper/dynamic buttons test (#44)
    * add dynamic buttons test
    
    * remove InterruptedException

---
+++ src/test/java/school/redrover/AqaGroupDynamicButtonsTest.java
@@ -0,0 +1,38 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class AqaGroupDynamicButtonsTest {
+
+    WebDriver driver;
+
+    @AfterMethod
+    public void close() {
+        if (driver != null) driver.quit();
+    }
+
+    @Test
+    public void simpleDynamicButtonsTest() {
+        driver = new ChromeDriver();
+        driver.get("https://testpages.eviltester.com/styled/dynamic-buttons-simple.html");
+
+        driver.findElement(By.id("button00")).click();
+        driver.findElement(By.id("button01")).click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("button02")));
+        driver.findElement(By.id("button02")).click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("button03")));
+        driver.findElement(By.id("button03")).click();
+
+        Assert.assertEquals(driver.findElement(By.id("buttonmessage")).getText(), "All Buttons Clicked", "Not All Buttons Clicked");
+    }
+}
