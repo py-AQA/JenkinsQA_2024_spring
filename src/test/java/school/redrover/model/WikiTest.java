6c1207002edf641ce5a058d64d72793abb1cf4ed
Eugene S <48071321+Ev9eny@users.noreply.github.com>
2024-03-24 19:22:19 -0400

    Create WikiTest.java

---
+++ src/test/java/school/redrover/model/WikiTest.java
@@ -0,0 +1,37 @@
+package school.redrover.model;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+import static org.testng.Assert.assertEquals;
+
+public class WikiTest {
+
+    @Test
+    public void seleniumTest() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.wikipedia.org/");
+
+        String title = driver.getTitle();
+        assertEquals(title, "Wikipedia");
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+
+        WebElement textBox = driver.findElement(By.id("searchInput"));
+        WebElement submitButton = driver.findElement(By.xpath("//*[@id=\"search-form\"]/fieldset/button"));
+
+        textBox.sendKeys("test");
+        submitButton.click();
+
+        WebElement message = driver.findElement(By.xpath("//*[@id=\"firstHeading\"]/span"));
+        String value = message.getText();
+        assertEquals(value, "Test");
+
+        driver.quit();
+    }
+}
