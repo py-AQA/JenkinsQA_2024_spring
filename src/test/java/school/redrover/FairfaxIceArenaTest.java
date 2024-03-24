c811c0bbe1e797b7fa4b4091fd5cae925e66e3d0
Elena Pozhydaieva <epozhydaieva@gmail.com>
2024-03-24 15:44:40 -0400

    Add my first java test

---
+++ src/test/java/school/redrover/FairfaxIceArenaTest.java
@@ -0,0 +1,30 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class FairfaxIceArenaTest {
+    @Test
+    public void checkRedirectionProShopTest() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+
+        driver.get("https://www.fairfaxicearena.com/");
+
+
+        WebElement proShopBtn = driver.findElement(By.xpath("//a[normalize-space()='Pro Shop']"));
+        proShopBtn.click();
+
+
+        String currentUrl = driver.getCurrentUrl();
+        Assert.assertEquals(currentUrl, "https://www.fairfaxicearena.com/pro-shop.html");
+
+        driver.quit();
+    }
+}
