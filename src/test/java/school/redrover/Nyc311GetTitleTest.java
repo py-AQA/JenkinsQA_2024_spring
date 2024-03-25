2acd4e037ea2b19ae1b886baf69c8a049a5b6822
Ana-Lara <analara1303@gmail.com>
2024-03-24 19:18:32 -0600

    Added my first test for NYC 311 website

---
+++ src/test/java/school/redrover/Nyc311GetTitleTest.java
@@ -0,0 +1,27 @@
+package school.redrover;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Nyc311GetTitleTest {
+
+        @Test
+        public static void main(String[] args) {
+            WebDriver driver = new ChromeDriver();
+            String baseUrl = "https://portal.311.nyc.gov/";
+            String expectedResult = "Home &nbsp;· NYC311";
+            String actualTitleResult = " ";
+
+            driver.get(baseUrl);
+            driver.getTitle();
+
+            Assert.assertEquals(actualTitleResult, expectedResult);
+
+            driver.quit();
+
+        }
+
+    }
+
