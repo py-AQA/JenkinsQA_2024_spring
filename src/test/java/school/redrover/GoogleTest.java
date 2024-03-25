f97828f52c5878ce0e669684b5a3ef0e42926ea1
Tetiana Rakhmanova <tetiana_rakhmanova@epam.com>
2024-03-25 15:51:03 -0700

    Add new test TR

--- src/test/java/school/redrover/GoogleTest.java
+++ src/test/java/school/redrover/GoogleTest.java
@@ -10,7 +10,7 @@ import static org.testng.Assert.assertEquals;
 
 public class GoogleTest {
     @Test
-    public void testGoogle() throws InterruptedException {
+    public void testGoogle() {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com");
 
