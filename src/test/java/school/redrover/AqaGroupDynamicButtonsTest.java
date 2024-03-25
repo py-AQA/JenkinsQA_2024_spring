5ee63f5a4982e6e0382c1909cff9e6b915d9a23c
aqaestuser <144237758+aqaestuser@users.noreply.github.com>
2024-03-25 15:10:55 +0300

    [!] optimize imports

--- src/test/java/school/redrover/AqaGroupDynamicButtonsTest.java
+++ src/test/java/school/redrover/AqaGroupDynamicButtonsTest.java
@@ -1,12 +1,9 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
