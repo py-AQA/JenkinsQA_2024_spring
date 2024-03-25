99ae4d34af64da953b0023c168e3034889160f7f
Larysa Shornikova <loryshornikova@gmail.com>
2024-03-25 08:32:41 +0300

    Add SearchTest

---
+++ src/test/java/school/redrover/model/SearchTest.java
@@ -0,0 +1,34 @@
+package school.redrover.model;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import java.time.Duration;
+
+ public class SearchTest {
+
+        @Test
+        public void testYandexSearch() {
+
+            WebDriver driver = new ChromeDriver();
+            driver.get("https://yandex.by");
+            driver.manage().timeouts().implicitlyWait(Duration.ofMillis(2000));
+//        WebElement closePopUp =  driver.findElement(By.xpath("/html/body/div[18]/button"));
+//        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(2000));
+//        closePopUp.click();
+            WebElement searchField = driver.findElement(By.xpath("//*[@id=\"text\"]"));
+            searchField.click();
+            searchField.sendKeys("RedRover School");
+            WebElement findButton = driver.findElement(By.xpath("/html/body/main/div[2]/form/div[3]/button"));
+            findButton.click();
+            WebElement schoolLink = driver.findElement(By.linkText("RedRover | Бесплатная IT-школа"));
+            String value = schoolLink.getText();
+            Assert.assertEquals("RedRover | Бесплатная IT-школа", value);
+            driver.quit();
+        }
+    }
+
+
