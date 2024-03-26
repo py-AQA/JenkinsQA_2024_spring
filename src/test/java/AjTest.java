6b7307fd7ba8963868f603481a409b8928f396b2
AnnaJabine <74692801+AnnaJabine@users.noreply.github.com>
2024-03-25 20:58:23 -0500

    Create new test

---
+++ src/test/java/AjTest.java
@@ -0,0 +1,36 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class AjTest {
+    @Test
+    public void testTest2(){
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+
+        //      String title = driver.getTitle();
+        //      Assert.assertEquals("Web form", title;
+
+        WebElement textBox = driver.findElement(By.name("my-text"));
+        WebElement submitButton = driver.findElement(By.cssSelector("button"));
+        WebElement texArea = driver.findElement(By.name("my-textarea"));
+        texArea.sendKeys("Anna!");
+
+        textBox.sendKeys("Selenium");
+        submitButton.click();
+
+        WebElement message = driver.findElement(By.id("message"));
+        String value = message.getText();
+        Assert.assertEquals("Received!", value);
+
+        driver.quit();
+
+    }
+}
