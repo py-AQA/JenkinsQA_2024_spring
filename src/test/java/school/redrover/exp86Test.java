9686dd56828904e05dbadfe6a0fb5de7cc0d1508
Evgenii_Tebenkov <evgeniitebenkov@aol.com>
2024-03-27 02:00:40 +0500

    Creat my 1 test

---
+++ src/test/java/school/redrover/exp86Test.java
@@ -0,0 +1,27 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class exp86Test {
+    @Test
+    public void testExp86() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://exp86.ru/");
+
+        WebElement textBox = driver.findElement(By.name("q"));
+        textBox.sendKeys("Hello world!");
+
+        WebElement registrationHrev = driver.findElement(By.linkText("Личный кабинет"));
+        registrationHrev.click();
+
+        WebElement text = driver.findElement(By.className("form_default_star"));
+        String value = text.getText();
+
+        Assert.assertEquals("* обязательные поля для заполнения", value);
+    }
+}
