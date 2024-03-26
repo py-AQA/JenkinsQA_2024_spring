1e158c5905f1e749c72717cf2bbad3fa6d62fc06
kirzhoy <kirzhoy@ya.ru>
2024-03-26 13:44:55 +0400

    add new test

---
+++ src/test/java/school/redrover/LookingForTheSummerTest.java
@@ -0,0 +1,26 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class LookingForTheSummerTest {
+    @Test
+    public void testLookingForTheSummer() {
+        WebDriver driver = new ChromeDriver();
+        WebDriverWait webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
+        driver.get("https://www.onlinetrade.ru/");
+        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@name = 'query']"))).sendKeys("лето");
+        driver.findElement(By.xpath("//input[@type = 'submit']")).click();
+        Assert.assertTrue((driver.findElement(By.xpath("//h1[contains(text(), 'Найденные товары')]")).isDisplayed()));
+
+        driver.quit();
+    }
+}
