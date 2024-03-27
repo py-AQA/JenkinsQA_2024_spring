e13c3ad6ade94a8963f5be66eb226900cd76bee3
aqaestuser <144237758+aqaestuser@users.noreply.github.com>
2024-03-27 22:29:14 +0300

    Aqa/uitestingplayground refactoring (#177)
    * [+] getDriver() and refactoring

--- src/test/java/school/redrover/AqaGroupBaseTest.java
+++ src/test/java/school/redrover/AqaGroupBaseTest.java
@@ -16,6 +16,8 @@ public class AqaGroupBaseTest {
     protected WebDriver driver;
     private WebDriverWait wait5;
     private WebDriverWait wait15;
+    private WebDriverWait wait25;
+    private WebDriverWait wait60;
 
     @BeforeMethod
     protected void setUp() {
@@ -24,6 +26,10 @@ public class AqaGroupBaseTest {
 //        chromeOptions.addArguments("--headless");
         chromeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
         driver = new ChromeDriver(chromeOptions);
+    }
+
+    protected WebDriver getDriver() {
+        return driver;
     }
 
     protected WebElement scrollIntoView(WebElement element) {
@@ -38,11 +44,25 @@ public class AqaGroupBaseTest {
         return wait5;
     }
 
-    protected WebDriverWait getWait15(){
+    protected WebDriverWait getWait15() {
         if (wait15 == null) {
             wait15 = new WebDriverWait(driver, Duration.ofSeconds(15));
         }
         return wait15;
+    }
+
+    protected WebDriverWait getWait25() {
+        if (wait25 == null) {
+            wait25 = new WebDriverWait(driver, Duration.ofSeconds(25));
+        }
+        return wait25;
+    }
+
+    protected WebDriverWait getWait60() {
+        if (wait60 == null) {
+            wait60 = new WebDriverWait(driver, Duration.ofSeconds(60));
+        }
+        return wait60;
     }
 
     @AfterMethod
@@ -51,5 +71,7 @@ public class AqaGroupBaseTest {
             driver.quit();
         wait5 = null;
         wait15 = null;
+        wait25 = null;
+        wait60 = null;
     }
 }
