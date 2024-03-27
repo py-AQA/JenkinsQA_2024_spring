21af913beaad0df14560ff62b2b8be2c4f4f768e
Tihhonix <tihhonix@gmail.com>
2024-03-28 01:27:34 +0400

    EG/UpdateTest2

--- src/test/java/school/redrover/ArMobiTest.java
+++ src/test/java/school/redrover/ArMobiTest.java
@@ -12,6 +12,11 @@ import org.testng.annotations.Test;
 
 public class ArMobiTest {
 
+    public static final String URL = "http://23.105.246.172:5000/login";
+    public static final String INPUT_PASSWORD = "//input[@class='ant-input']";
+    public static final String INPUT_EMAIL = "//input[@class='ant-input primaryInput  not-entered']";
+    public static final String BTN_PASSWORD = "//button[@class='ant-btn ant-btn-default authButton big colorPrimary ']";
+    public static final String EMAIL = "n-k-65@list.ru";
     private final By getPaswordText = By.xpath("//h2[@class='ant-typography h2_m RestorePassword__sendSuccess-text'][contains(.,'Мы отправили по адресу')]");
 
     private final By getErrorText = By.xpath("//div[@style='text-align: center; margin-bottom: 20px; color: rgb(255, 0, 0);']");
@@ -20,7 +25,7 @@ public class ArMobiTest {
 
     @BeforeTest
     public void initWebDriver() {
-        driver = new ChromeDriver();
+//        driver = new ChromeDriver();
         driver.manage().window().setSize(new Dimension(1820,1080));
     }
 
@@ -31,14 +36,13 @@ public class ArMobiTest {
     @Test
     public void removePaswordTest() throws InterruptedException {
 
-        driver.get("http://23.105.246.172:5000/login");
+        driver.get(URL);
 
         Thread.sleep(1000);
 
-        WebElement inputMail = driver.findElement(By.xpath("//input[@class='ant-input primaryInput  not-entered']"));
-        inputMail.sendKeys("yyyyyyyyyy@mail.xx");
+        driver.findElement(By.xpath(INPUT_EMAIL)).sendKeys("yyyyyyyyyy@mail.xx");
 
-        driver.findElement(By.xpath("//button[@class='ant-btn ant-btn-default authButton big colorPrimary ']")).click();
+        driver.findElement(By.xpath(BTN_PASSWORD)).click();
         Thread.sleep(1000);
 
         String getError = driver.findElement(getErrorText).getText();
@@ -50,15 +54,14 @@ public class ArMobiTest {
     @Test
     public void regaTest() throws InterruptedException {
 
-        driver.get("http://23.105.246.172:5000/login");
+        driver.get(URL);
         Thread.sleep(1000);
 
         driver.findElement(By.xpath("//h2[@class='ant-typography h2_m Login__restore-text']")).click();
 
-        WebElement inputMail = driver.findElement(By.xpath("//input[@class='ant-input primaryInput  not-entered']"));
-        inputMail.sendKeys("n-k-65@list.ru");
+        driver.findElement(By.xpath(INPUT_EMAIL)).sendKeys(EMAIL);
 
-        driver.findElement(By.xpath("//button[@class='ant-btn ant-btn-default authButton big colorPrimary ']")).click();
+        driver.findElement(By.xpath(BTN_PASSWORD)).click();
         Thread.sleep(1000);
 
         String getPasError = driver.findElement(getPaswordText).getText();
