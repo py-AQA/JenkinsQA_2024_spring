f54a4530c375c95f3a136c03e670defc219de435
VyacheslavBeliaev <vbelyaev86@gmail.com>
2024-03-28 20:40:45 +0300

    Add_Test DoubleClick

---
+++ src/test/java/school/redrover/CodeMagazineTasksAreaTest.java
@@ -0,0 +1,33 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CodeMagazineTasksAreaTest {
+
+    @Test
+    public void testTasksArea() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://thecode.media/");
+
+        WebElement search_area = driver.findElement(By.className("tab-questions"));
+
+        Action myAction = new Actions(driver).doubleClick(search_area).build();
+        myAction.perform();
+
+        WebElement found_text = driver.findElement(By.xpath("(//h1[@class='search__title'])"));
+        String found_search_title = found_text.getText();
+
+        Assert.assertEquals("Как решить", found_search_title);
+
+        driver.quit();
+
+    }
+
+}
