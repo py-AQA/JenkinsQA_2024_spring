5316284637c7417d60b9bfa14f2725451260123a
Elena <panfilova.msk@gmail.com>
2024-03-28 16:08:39 +0700

    Add my test

---
+++ src/test/java/school/redrover/JilfondTest.java
@@ -0,0 +1,22 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class JilfondTest {
+
+    @Test
+    public void TestJilfond() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://jilfond.ru/");
+
+        WebElement text = driver.findElement(By.name("price[to]"));
+        text.sendKeys("3000");
+
+        WebElement button = driver.findElement(By.id("js-search-map"));
+        button.click();
+    }
+}
