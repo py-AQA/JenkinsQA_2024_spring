e6552865fc8cba511d1dd8671ddbe8c969058c8c
Halyna A <125617798+7108581@users.noreply.github.com>
2024-03-27 19:29:59 -0700

    Forward Group test Costco search field (#187)

---
+++ src/test/java/school/redrover/model/GroupForwardTest.java
@@ -0,0 +1,24 @@
+package school.redrover.model;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class GroupForwardTest {
+
+    @Test
+    public void testCostco() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.costco.com/");
+        driver.findElement(By.xpath("//*[@id='search-field']")).sendKeys("apple");
+        driver.findElement(By.xpath("//*[@id='search-field']")).sendKeys(Keys.ENTER);
+
+        Assert.assertEquals(driver.findElement(By.xpath("//*[@id='crumbs_ul']/li[2]/span")).getText(), "Apple Products");
+
+        driver.quit();
+    }
+}
+
