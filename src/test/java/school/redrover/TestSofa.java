aea9662ef67b2c69ce9d3ec650bc3eca0f922bf9
Aliaksandr Dubouski <43587497+sashokdzr@users.noreply.github.com>
2024-03-28 11:15:03 +0300

    Update TestSofa.java

--- src/test/java/school/redrover/TestSofa.java
+++ src/test/java/school/redrover/TestSofa.java
@@ -7,15 +7,18 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class TestSofa {
+public class SofaTest {
     @Test
     public void testBadLogin() throws InterruptedException {
         WebDriver driver = new ChromeDriver();
+        
         driver.get("https://www.sofa.com/gb");
         Assert.assertEquals(driver.getTitle(), "Sofas - Handmade To Order, Especially for you | Sofa.com");
+        
         WebElement loginButton = driver.findElement(By.xpath("//a[@href=\"/gb/login\"]"));
         loginButton.click();
         Thread.sleep(1000);
+        
         WebElement inputEmail = driver.findElement(By.id("j_username"));
         inputEmail.sendKeys("hello@gmail.com");
         WebElement inputPassword = driver.findElement(By.id("j_password"));
@@ -23,10 +26,12 @@ public class TestSofa {
         WebElement loginBtn = driver.findElement(By.id("loginBtn"));
         loginBtn.click();
         Thread.sleep(1000);
+        
         WebElement errorText = driver.findElement(By.xpath("//div[@class=\"alert alert-danger alert-dismissable\"]"));
         String text = errorText.getText();
         Assert.assertEquals(text, "×\n" +
                 "Your username or password was incorrect.");
+        
         driver.quit();
 
     }
