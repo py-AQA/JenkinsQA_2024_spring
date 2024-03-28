10ed0b806dbba35316833f49260c1ed8237cbbb9
Evgeniy <119495875+Gaztechica@users.noreply.github.com>
2024-03-28 11:24:40 +0400

    addtest (#195)

---
+++ src/test/java/school/redrover/ArSoftTest.java
@@ -0,0 +1,13 @@
+package school.redrover;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+public class ArSoftTest {
+
+    @Test
+    public void testReg() {
+
+
+    }
+}
