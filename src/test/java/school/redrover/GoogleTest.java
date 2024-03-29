7c00afee074cbd22095897f74f1e940af24cf924
Sergey.D <sergei.demyanenko@gmail.com>
2024-03-28 22:31:13 -0400

    Refactoring 1

--- src/test/java/school/redrover/GoogleTest.java
+++ src/test/java/school/redrover/GoogleTest.java
@@ -18,7 +18,7 @@ public class GoogleTest {
         WebElement text = driver.findElement(By.id("APjFqb"));
         text.sendKeys("Selenium");
 
-        Thread.sleep(1000);
+        Thread.sleep(1000); // looks not good
 
 
         WebElement button = driver.findElement(By.className("gNO89b"));
