eb3a6b87cd7f16e742a1d8af751d99aa0730d8cc
Evgeniy <119495875+Gaztechica@users.noreply.github.com>
2024-03-29 14:08:48 +0400

    eg/updateTest (#249)

--- src/test/java/school/redrover/ArMobileTest.java
+++ src/test/java/school/redrover/ArMobileTest.java
@@ -1,21 +1,19 @@
 package school.redrover;
 
 import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-//import org.testng.annotations.AfterMethod;
-//import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
+import school.redrover.runner.BaseTest;
 import java.util.ArrayList;
 
-public class ArMobileTest {
 
-    public static final String URL = "http://23.105.246.172:5000/login";
-    public static final String INPUT_PASSWORD = "//input[@class='ant-input']";
-    public static final String INPUT_EMAIL = "//input[@class='ant-input primaryInput  not-entered']";
-    public static final String BTN_PASSWORD = "//button[@class='ant-btn ant-btn-default authButton big colorPrimary ']";
-    public static final String EMAIL = "n-k-65@list.ru";
+public class ArMobileTest extends BaseTest {
+
+    private static final String URL = "http://23.105.246.172:5000/login";
+    private static final String INPUT_PASSWORD = "//input[@class='ant-input']";
+    private static final String INPUT_EMAIL = "//input[@class='ant-input primaryInput  not-entered']";
+    private static final String BTN_PASSWORD = "//button[@class='ant-btn ant-btn-default authButton big colorPrimary ']";
+    private static final String EMAIL = "n-k-65@list.ru";
     private final By getPaswordText = By.xpath("//h2[@class='ant-typography h2_m RestorePassword__sendSuccess-text'][contains(.,'Мы отправили по адресу')]");
     private final By getErrorText = By.xpath("//div[@style='text-align: center; margin-bottom: 20px; color: rgb(255, 0, 0);']");
     private final By newProgectNameText = By.xpath("//div[@class='Sidebar__project-name'][contains(.,'1Новый проект')]");
@@ -23,212 +21,171 @@ public class ArMobileTest {
     private final By getPoliticaUserText = By.xpath("//span[@style='font-size: 19px;'][contains(.,'Предмет пользовательского соглашения')]");
     private final By getBotText = By.xpath("//span[@dir='auto']");
 
-    WebDriver driver = new ChromeDriver();
-
-//    @BeforeMethod
-//    @AfterMethod
 
     @Test
     public void testRemovePasword() throws InterruptedException {
 
-        driver.get(URL);
-        driver.manage().window().setSize(new Dimension(1920,1080));
+        getDriver().get(URL);
+        getDriver().manage().window().setSize(new Dimension(1920,1080));
         Thread.sleep(1000);
 
-        driver.findElement(By.xpath(INPUT_EMAIL)).sendKeys("yyyyyyyyyy@mail.xx");
+        getDriver().findElement(By.xpath(INPUT_EMAIL)).sendKeys("yyyyyyyyyy@mail.xx");
 
-        driver.findElement(By.xpath(BTN_PASSWORD)).click();
+        getDriver().findElement(By.xpath(BTN_PASSWORD)).click();
         Thread.sleep(1000);
 
-        String getError = driver.findElement(getErrorText).getText();
+        String getError = getDriver().findElement(getErrorText).getText();
 
         Assert.assertEquals(getError,"Неправильный логин или пароль");
-
-        driver.quit();
     }
 
     @Test
     public void testRega() throws InterruptedException {
 
-        driver.get(URL);
-        driver.manage().window().setSize(new Dimension(1920,1080));
+        getDriver().get(URL);
+        getDriver().manage().window().setSize(new Dimension(1920,1080));
         Thread.sleep(1000);
 
-        driver.findElement(By.xpath("//h2[@class='ant-typography h2_m Login__restore-text']")).click();
+        getDriver().findElement(By.xpath("//h2[@class='ant-typography h2_m Login__restore-text']")).click();
 
-        driver.findElement(By.xpath(INPUT_EMAIL)).sendKeys(EMAIL);
+        getDriver().findElement(By.xpath(INPUT_EMAIL)).sendKeys(EMAIL);
 
-        driver.findElement(By.xpath(BTN_PASSWORD)).click();
+        getDriver().findElement(By.xpath(BTN_PASSWORD)).click();
         Thread.sleep(1000);
 
-        String getPasError = driver.findElement(getPaswordText).getText();
+        String getPasError = getDriver().findElement(getPaswordText).getText();
 
         Assert.assertEquals(getPasError,"Мы отправили по адресу n-k-65@list.ru ссылку для восстановления доступа");
-
-        driver.quit();
     }
 
     @Test
     public void testHrefPoluticUser() throws InterruptedException{
 
-        driver.get(URL);
-        driver.manage().window().setSize(new Dimension(1920,1080));
+        getDriver().get(URL);
+
+        getDriver().manage().window().setSize(new Dimension(1920,1080));
         Thread.sleep(1000);
 
-        driver.findElement(By.xpath("//a[@href='https://vr-arsoft.com/user-agreement-armobail/']")).click();
+        getDriver().findElement(By.xpath("//a[@href='https://vr-arsoft.com/user-agreement-armobail/']")).click();
 
-        ArrayList<String> newTab = new ArrayList<>(driver.getWindowHandles());
-        driver.switchTo().window(newTab.get(1));
+        ArrayList<String> newTab = new ArrayList<>(getDriver().getWindowHandles());
+        getDriver().switchTo().window(newTab.get(1));
         Thread.sleep(1000);
 
-        String getPoliticaUser = driver.findElement(getPoliticaUserText).getText();
+        String getPoliticaUser = getDriver().findElement(getPoliticaUserText).getText();
 
         Assert.assertEquals("Предмет пользовательского соглашения", getPoliticaUser);
-        driver.quit();
+        getDriver().quit();
     }
 
     @Test
     public void testCreateProgect() throws InterruptedException {
 
-        driver.get(URL);
-
-        driver.manage().window().setSize(new Dimension(1920,1080));
-        Thread.sleep(1000);
-        driver.findElement(By.xpath(INPUT_EMAIL)).sendKeys(EMAIL);
-        driver.findElement(By.xpath(INPUT_PASSWORD)).sendKeys("qwe13567");
-        driver.findElement(By.xpath(BTN_PASSWORD)).click();
-//  сайт тормоз не убирать sleep ================
-        Thread.sleep(9000);
-//=================================
-        WebElement input = driver.findElement(By.xpath("//button[@class='ant-btn ant-btn-default primaryButton big colorPrimary ']"));
-        input.click();
+        getDriver().get(URL);
+        getDriver().manage().window().setSize(new Dimension(1920,1080));
         Thread.sleep(1000);
 
-        WebElement inp = driver.findElement(By.xpath("//input[@class='ant-input primaryInput  not-entered']"));
-        inp.sendKeys("1Новый проект");
+        getDriver().findElement(By.xpath(INPUT_EMAIL)).sendKeys(EMAIL);
+        getDriver().findElement(By.xpath(INPUT_PASSWORD)).sendKeys("qwe13567");
+        getDriver().findElement(By.xpath(BTN_PASSWORD)).click();
+
+        Thread.sleep(9000); //  сайт тормоз не убирать sleep ===
+
+
+        getDriver().findElement(By.xpath("//button[@class='ant-btn ant-btn-default primaryButton big colorPrimary ']")).click();
         Thread.sleep(1000);
 
-        WebElement inp1 =driver.findElement(By.xpath("//input[@id='CreateProjectForm_city']"));
-        inp1.sendKeys("Самара");
+        getDriver().findElement(By.xpath("//input[@class='ant-input primaryInput  not-entered']")).sendKeys("1Новый проект");
         Thread.sleep(1000);
 
-        WebElement inp2 =driver.findElement(By.xpath("//input[@id='CreateProjectForm_country']"));
-        inp2.sendKeys("РФ");
+        getDriver().findElement(By.xpath("//input[@id='CreateProjectForm_city']")).sendKeys("Самара");
         Thread.sleep(1000);
 
-        WebElement inp3 =driver.findElement(By.xpath("//textarea[@id='CreateProjectForm_street']"));
-        inp3.sendKeys("Победы");
+        getDriver().findElement(By.xpath("//input[@id='CreateProjectForm_country']")).sendKeys("РФ");
         Thread.sleep(1000);
 
-        WebElement inp4 =driver.findElement(By.xpath("//input[@id='CreateProjectForm_postalCode']"));
-        inp4.sendKeys("444444");
+        getDriver().findElement(By.xpath("//textarea[@id='CreateProjectForm_street']")).sendKeys("Победы");
         Thread.sleep(1000);
 
-        WebElement inp5 =driver.findElement(By.xpath("//input[@id='CreateProjectForm_code']"));
-        inp5.sendKeys("123");
+        getDriver().findElement(By.xpath("//input[@id='CreateProjectForm_postalCode']")).sendKeys("444444");
         Thread.sleep(1000);
 
-        WebElement inp6 =driver.findElement(By.xpath("//input[@id='CreateProjectForm_building']"));
-        inp6.sendKeys("121");
+        getDriver().findElement(By.xpath("//input[@id='CreateProjectForm_code']")).sendKeys("123");
         Thread.sleep(1000);
 
-        WebElement inp7 =driver.findElement(By.xpath("//input[@id='CreateProjectForm_office']"));
-        inp7.sendKeys("117");
+        getDriver().findElement(By.xpath("//input[@id='CreateProjectForm_building']")).sendKeys("121");
         Thread.sleep(1000);
 
-        WebElement inp8 =driver.findElement(By.xpath("//input[@id='CreateProjectForm_startDate']"));
-        inp8.click();
+        getDriver().findElement(By.xpath("//input[@id='CreateProjectForm_office']")).sendKeys("117");
         Thread.sleep(1000);
 
-        WebElement NewData = driver.findElement(By.xpath("//a[@class='ant-picker-today-btn']"));
+        getDriver().findElement(By.xpath("//input[@id='CreateProjectForm_startDate']")).click();
+        Thread.sleep(1000);
+
+        WebElement NewData = getDriver().findElement(By.xpath("//a[@class='ant-picker-today-btn']"));
         Thread.sleep(1000);
         NewData.click();
 
-        WebElement EndData = driver.findElement(By.xpath("//input[@id='CreateProjectForm_endDate']"));
+        WebElement EndData = getDriver().findElement(By.xpath("//input[@id='CreateProjectForm_endDate']"));
         EndData.sendKeys("26-05-2024");
         Thread.sleep(1000);
-
         EndData.sendKeys(Keys.ENTER);
 
-        WebElement btn = driver.findElement(By.xpath("//button[@class='ant-btn ant-btn-default primaryButton big colorPrimary ']"));
-        btn.click();
+        getDriver().findElement(By.xpath("//button[@class='ant-btn ant-btn-default primaryButton big colorPrimary ']")).click();
         Thread.sleep(4000);
 
-        WebElement btnD = driver.findElement(By.xpath("//div[@class='ant-typography ant-typography-ellipsis ant-typography-single-line ant-typography-ellipsis-single-line p_r']"));
-        btnD.click();
+        getDriver().findElement(By.xpath("//div[@class='ant-typography ant-typography-ellipsis ant-typography-single-line ant-typography-ellipsis-single-line p_r']")).click();
         Thread.sleep(2000);
 
-//       ==============================кнопка отмены ===========================
+        getDriver().findElement(By.xpath("//a[@style='color: inherit;'][contains(.,'Настройки')]")).click();
 
-//          WebElement textBoxAside = driver.findElement(By.xpath("//button[@class='Sidebar__collapsibleBtn']"));
-//          textBoxAside.click();
-//          Thread.sleep(3000);
-
-        WebElement textSection = driver.findElement(By.xpath("//a[@style='color: inherit;'][contains(.,'Настройки')]"));
-        textSection.click();
-
-//        =========================== свернутый сайтбар ===============================
-
-//           WebElement textSectionInput = driver.findElement(By.xpath("//input[@value='1Новый проект']"));
-//           textSectionInput.click();
-//           Thread.sleep(4000);
-
-
-
-        String newProgectName = driver.findElement(newProgectNameText).getText();
+        String newProgectName = getDriver().findElement(newProgectNameText).getText();
 
         Assert.assertEquals("1Новый проект", newProgectName);
 
-        WebElement btnDt = driver.findElement(By.xpath("//button[@class='ant-btn ant-btn-default primaryButton big colorRed ']"));
-        btnDt.click();
+        getDriver().findElement(By.xpath("//button[@class='ant-btn ant-btn-default primaryButton big colorRed ']")).click();
         Thread.sleep(2000);
 
-
-
-        WebElement btnDel = driver.findElement(By.xpath("//button[@class='ant-btn ant-btn-default primaryButton big colorPrimary '][contains(.,'Подтвердить')]"));
-        btnDel.click();
-
-        driver.quit();
+        getDriver().findElement(By.xpath("//button[@class='ant-btn ant-btn-default primaryButton big colorPrimary '][contains(.,'Подтвердить')]")).click();
     }
 
     @Test
     public void testHrefPolitic() throws InterruptedException {
 
-        driver.get(URL);
-        driver.manage().window().setSize(new Dimension(1920,1080));
-        Thread.sleep(1000);
-        driver.findElement(By.xpath("//a[@href='https://vr-arsoft.com/personal-data-processing-policy/']")).click();
+        getDriver().get(URL);
+        getDriver().manage().window().setSize(new Dimension(1920,1080));
         Thread.sleep(1000);
 
-        ArrayList<String> newTab = new ArrayList<>(driver.getWindowHandles());
-        driver.switchTo().window(newTab.get(1));
-        String getErr = driver.findElement(getPoliticaText).getText();
+        getDriver().findElement(By.xpath("//a[@href='https://vr-arsoft.com/personal-data-processing-policy/']")).click();
+        Thread.sleep(1000);
+
+        ArrayList<String> newTab = new ArrayList<>(getDriver().getWindowHandles());
+        getDriver().switchTo().window(newTab.get(1));
+        String getErr = getDriver().findElement(getPoliticaText).getText();
 
         Assert.assertEquals("Политика обработки персональных данных", getErr);
-        driver.quit();
     }
 
     @Test
     public void testHrefBot() throws InterruptedException {
 
-        driver.get(URL);
-        driver.manage().window().setSize(new Dimension(1920,1080));
+        getDriver().get(URL);
+        getDriver().manage().window().setSize(new Dimension(1920,1080));
         Thread.sleep(3000);
 
-        driver.findElement(By.xpath(INPUT_EMAIL)).sendKeys(EMAIL);
+        getDriver().findElement(By.xpath(INPUT_EMAIL)).sendKeys(EMAIL);
 
-        driver.findElement(By.xpath(INPUT_PASSWORD)).sendKeys("qwe13567");
-        driver.findElement(By.xpath(BTN_PASSWORD)).click();
+        getDriver().findElement(By.xpath(INPUT_PASSWORD)).sendKeys("qwe13567");
+        getDriver().findElement(By.xpath(BTN_PASSWORD)).click();
         Thread.sleep(1000);
 
-        driver.findElement(By.xpath("//a[@href='https://t.me/arsoft_support_bot']")).click();
+        getDriver().findElement(By.xpath("//a[@href='https://t.me/arsoft_support_bot']")).click();
         Thread.sleep(1000);
 
-        ArrayList<String> newTab = new ArrayList<>(driver.getWindowHandles());
-        driver.switchTo().window(newTab.get(1));
-        String getBot = driver.findElement(getBotText).getText();
+        ArrayList<String> newTab = new ArrayList<>(getDriver().getWindowHandles());
+        getDriver().switchTo().window(newTab.get(1));
+        String getBot = getDriver().findElement(getBotText).getText();
 
         Assert.assertEquals("AR SOFT support", getBot);
-        driver.quit();
     }
 }
