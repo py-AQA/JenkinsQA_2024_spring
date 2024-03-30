894a3fd6f3e9cab852bd1d5c1f72c27e6e1e4ca5
Ivan Denisenko <info@sweetscripts.org>
2024-03-29 22:49:50 -0400

    add a group

--- src/test/java/school/redrover/TeaPartyGroupTest.java
+++ src/test/java/school/redrover/TeaPartyGroupTest.java
@@ -1,11 +1,27 @@
 package school.redrover;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TeaPartyGroupTest {
 
     @Test
-    public void ivansTest() {
+    public void idDemoTest() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+
+        WebElement login = driver.findElement(By.id("user-name"));
+        login.sendKeys("standard_user");
+        WebElement loginPassword = driver.findElement(By.name("password"));
+        loginPassword.sendKeys("secret_sauce");
+        WebElement loginButton = driver.findElement(By.id("login-button"));
+        loginButton.click();
+        WebElement menu = driver.findElement(By.id("react-burger-menu-btn"));
+        menu.click();
 
     }
 
