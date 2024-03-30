d4d27bb2c1962fbf6df4b9281a65e5dff133ee49
Serhii Malko <malsv@ukr.net>
2024-03-30 18:32:09 +0200

    Optimize import

--- src/test/java/school/redrover/ByteBustersGroupTest.java
+++ src/test/java/school/redrover/ByteBustersGroupTest.java
@@ -2,9 +2,7 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
