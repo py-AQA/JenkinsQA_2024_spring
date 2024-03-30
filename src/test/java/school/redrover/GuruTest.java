d7668307855540ce436f681a07736c62424f0888
Valentin Podkova <val.podkova@gmail.com>
2024-03-30 16:54:26 +0200

    refactor my new test

--- src/test/java/school/redrover/GuruTest.java
+++ src/test/java/school/redrover/GuruTest.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+
+
 import java.time.Duration;
 
 public class GuruTest {
