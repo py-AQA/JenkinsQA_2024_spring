b0f960e5cc8de2f2df57d171c2198a70e9f703d7
Alena <alenavoloshina27@gmail.com>
2024-03-29 22:49:44 -0400

    add alenaTest

--- src/test/java/school/redrover/TeaPartyGroupTest.java
+++ src/test/java/school/redrover/TeaPartyGroupTest.java
@@ -1,5 +1,10 @@
 package school.redrover;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TeaPartyGroupTest {
@@ -8,8 +13,21 @@ public class TeaPartyGroupTest {
     public void ivansTest() {
 
     }
+    @Test
+    public void alenaTest(){
+        WebDriver driver = new ChromeDriver();
+       driver.get("https://www.saucedemo.com/");
 
-
-
+        WebElement login = driver.findElement(By.id("user-name"));
+        login.sendKeys("standard_user");
+        WebElement loginPassword = driver.findElement(By.name("password"));
+        loginPassword.sendKeys("secret_sauce");
+        WebElement loginButton = driver.findElement(By.id("login-button"));
+        loginButton.click();
+        WebElement menu = driver.findElement(By.id("react-burger-menu-btn"));
+        String nMenu = menu.getText();
+        assert menu.getText().equals(nMenu) : "All Items";
+        menu.click();
+    }
 
 }
