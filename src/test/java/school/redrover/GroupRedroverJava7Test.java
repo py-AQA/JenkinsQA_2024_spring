fa0eda8d763a2561a971b208f4d49c0fb66268f5
irina-kuts <irynakuts14@gmail.com>
2024-03-30 16:19:16 -0500

    Deleted my first test and created a new one in my group's class

--- src/test/java/school/redrover/GroupRedroverJava7Test.java
+++ src/test/java/school/redrover/GroupRedroverJava7Test.java
@@ -2,9 +2,7 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
