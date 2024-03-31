c2d3583954781083155191c144231d6eca813c1c
AnkaVedmocka <anna_91@list.ru>
2024-03-31 19:35:11 +0300

    Test about Books

--- src/test/java/school/redrover/GroupAqaQuaQuaTest.java
+++ src/test/java/school/redrover/GroupAqaQuaQuaTest.java
@@ -1,26 +1,35 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
+import java.time.Duration;
+
 public class GroupAqaQuaQuaTest extends BaseTest {
    @Test
-    public void bookTest() {
+    public void testAddingBookToCart() {
         getDriver().get("https://demowebshop.tricentis.com/");
 
-        WebElement books = getDriver().findElement(By.xpath("/html/body/div[4]/div[1]/div[2]/ul[1]/li[1]/a"));
+
+        WebElement books = getDriver().findElement(By.xpath("//ul[@class='top-menu']//a[@href='/books']"));
         books.click();
 
-        WebElement myBook = getDriver().findElement(By.xpath("/html/body/div[4]/div[1]/div[4]/div[2]/div[2]/div[2]/div[3]/div[1]/div/div[2]/h2/a"));
+        WebElement myBook = getDriver().findElement(By.xpath("//div[@class = 'details']//a[@href= '/computing-and-internet']"));
         myBook.click();
 
         WebElement addMyBook = getDriver().findElement(By.id("add-to-cart-button-13"));
         addMyBook.click();
 
-        WebElement shoppingcart = getDriver().findElement(By.className("cart-label"));
-        shoppingcart.click();
+        WebElement shoppingСart = getDriver().findElement(By.className("cart-label"));
+        shoppingСart.click();
+
+
+       Assert.assertEquals(getDriver().findElement(By.className("product-name")), "Computing and Internet");
 
     }
 
