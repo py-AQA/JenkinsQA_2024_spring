5ccd3e695001426c514d47fc2e78bb0422819684
AnkaVedmocka <anna_91@list.ru>
2024-03-31 16:46:04 +0300

    Test about Books

---
+++ src/test/java/school/redrover/GroupAqaQuaQuaTest.java
@@ -0,0 +1,28 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+public class GroupAqaQuaQuaTest extends BaseTest {
+   @Test
+    public void bookTest() {
+        getDriver().get("https://demowebshop.tricentis.com/");
+
+        WebElement books = getDriver().findElement(By.xpath("/html/body/div[4]/div[1]/div[2]/ul[1]/li[1]/a"));
+        books.click();
+
+        WebElement myBook = getDriver().findElement(By.xpath("/html/body/div[4]/div[1]/div[4]/div[2]/div[2]/div[2]/div[3]/div[1]/div/div[2]/h2/a"));
+        myBook.click();
+
+        WebElement addMyBook = getDriver().findElement(By.id("add-to-cart-button-13"));
+        addMyBook.click();
+
+        WebElement shoppingcart = getDriver().findElement(By.className("cart-label"));
+        shoppingcart.click();
+
+    }
+
+}
