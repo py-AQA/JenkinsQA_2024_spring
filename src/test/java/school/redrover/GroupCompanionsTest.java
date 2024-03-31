3e8e9e2316b1550230dcf3d9df44d271c93c2548
IanaBlinova <blinovaianaa@gmail.com>
2024-03-31 14:30:57 +0500

    Add new test

--- src/test/java/school/redrover/GroupCompanionsTest.java
+++ src/test/java/school/redrover/GroupCompanionsTest.java
@@ -26,4 +26,30 @@ public class GroupCompanionsTest extends BaseTest {
 
         Assert.assertEquals("* обязательные поля для заполнения", value);
     }
+
+    @Test
+    public void Firsttest() throws InterruptedException {
+
+        WebDriver driver = getDriver();
+        driver.get("https://exp86.ru");
+
+        Thread.sleep(1000); //добавила для себя, чтобы было видно на тесте
+
+        WebElement test = driver.findElement(By.xpath("//input[@type='text']"));
+        test.sendKeys("Привет, мир!");
+
+        WebElement buttonSearch = driver.findElement(By.name("Поиск"));
+        buttonSearch.click();
+
+        Thread.sleep(1000);
+
+        WebElement button = driver.findElement(By.xpath("//a[@href='/user/']"));
+        button.click();
+
+        WebElement string = driver.findElement(By.className("form_default_star"));
+        String result = string.getText();
+
+        Assert.assertEquals("* обязательные поля для заполнения", result);
+    }
+
 }
