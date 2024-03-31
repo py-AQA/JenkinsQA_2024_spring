2eb740c4e4df1c41d7624224c27686ca92a27746
Alex <sahok200888@gmail.com>
2024-03-31 23:55:26 +0300

    Move my test into group package Refactoring

--- src/test/java/school/redrover/JavaHashGroupTest.java
+++ src/test/java/school/redrover/JavaHashGroupTest.java
@@ -3,7 +3,6 @@ package school.redrover;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
