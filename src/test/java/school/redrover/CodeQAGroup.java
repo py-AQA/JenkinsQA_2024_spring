3235d58b272f005fb5f2170d32171f7b4fb51df9
Viktoriia <yvika2909@gmail.com>
2024-03-31 18:00:03 +0300

    Add new test

---
+++ src/test/java/school/redrover/CodeQAGroup.java
@@ -0,0 +1,35 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+public class CodeQAGroup extends BaseTest {
+
+    @Test
+    public void TestSlider() {
+//        WebDriver driver = getDriver();
+        getDriver().get("https://the-internet.herokuapp.com/horizontal_slider");
+
+        WebElement slider = getDriver().findElement(By.xpath("/html/body/div[2]/div/div/div/input"));
+        WebElement text = getDriver().findElement(By.xpath("//*[@id='range']"));
+
+        double expectedValue = 0;
+        Assert.assertEquals(Double.parseDouble(text.getText()), expectedValue);
+
+        final double step = 0.5;
+        for (int i = 0; i < 10; i++) {
+            slider.sendKeys(Keys.ARROW_RIGHT);
+            expectedValue += step;
+            Assert.assertEquals(Double.parseDouble(text.getText()), expectedValue);
+        }
+        for (int i = 0; i < 11; i++) {
+            Assert.assertEquals(Double.parseDouble(text.getText()), expectedValue);
+            slider.sendKeys(Keys.ARROW_LEFT);
+            expectedValue -= step;
+        }
+    }
+}
