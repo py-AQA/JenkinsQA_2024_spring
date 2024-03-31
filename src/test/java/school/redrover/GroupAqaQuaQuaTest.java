4145c5579877b21f6985f048fd1b6152cd546e19
AnkaVedmocka <anna_91@list.ru>
2024-03-31 16:46:44 +0300

    Test about Books

--- src/test/java/school/redrover/GroupAqaQuaQuaTest.java
+++ src/test/java/school/redrover/GroupAqaQuaQuaTest.java
@@ -2,7 +2,6 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
