a220f3ccc97c98e8151e0ce2e9eb2e3cea140f1e
Anush Thompson <anushthompson@gmail.com>
2024-04-01 00:07:35 -0400

    Add new test

--- src/test/java/school/redrover/LegionOfJavaGroupTest.java
+++ src/test/java/school/redrover/LegionOfJavaGroupTest.java
@@ -2,6 +2,7 @@ package school.redrover;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -127,6 +128,23 @@ public class LegionOfJavaGroupTest extends BaseTest {
 
         assertTrue(homeWebElement.isDisplayed());
         assertTrue(homeWebElement.isEnabled());
+    }
+
+    @Test
+
+    public void testLogin(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://practicetestautomation.com/practice-test-login/");
+        WebElement userName = driver.findElement(By.name("username"));
+        userName.sendKeys("student");
+        WebElement password = driver.findElement(By.name("password"));
+        password.sendKeys("Password123");
+        WebElement submitButton = driver.findElement(By.id("submit"));
+        submitButton.click();
+        WebElement result = driver.findElement(By.xpath("//h1"));
+        String resultText = result.getText();
+        Assert.assertEquals("Logged In Successfully", resultText);
+        driver.quit();
     }
 
 }
