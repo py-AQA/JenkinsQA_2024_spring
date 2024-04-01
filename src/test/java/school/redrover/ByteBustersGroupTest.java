ca197d85027b85663d90f0c0e8cff2aad828c0c6
Ki1ativ <ki1ativ1992@yandex.ru>
2024-04-01 15:38:36 +0300

    PL HW 18

--- src/test/java/school/redrover/ByteBustersGroupTest.java
+++ src/test/java/school/redrover/ByteBustersGroupTest.java
@@ -146,4 +146,32 @@ public class ByteBustersGroupTest extends BaseTest {
         Assert.assertEquals(translationFieldText.getText(), "Привет, мир");
 
     }
+
+    @Test
+    public void testMarvel() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.marvel.com/");
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+
+        WebElement buttonAccept= driver.findElement(By.id("onetrust-accept-btn-handler"));
+        buttonAccept.click();
+
+        WebElement buttonSearch= driver.findElement(By.id("search"));
+        buttonSearch.click();
+
+        WebElement text = driver.findElement(By.className("typeahead__input"));
+        text.sendKeys("Deadpool");
+
+        text.sendKeys(Keys.ENTER);
+
+        Thread.sleep(1000);
+
+        WebElement link= driver.findElement(By.xpath ("//a[text()='Deadpool (Wade Wilson)'][@href='/characters/deadpool-wade-wilson']"));
+        String resultText = link.getText();
+
+        Assert.assertEquals(resultText, "Deadpool (Wade Wilson)");
+
+        driver.quit();
+    }
 }
