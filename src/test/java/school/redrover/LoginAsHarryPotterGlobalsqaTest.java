3fb28ad0dc22debab34ae544dfa6dff03745921e
DaryaDubrovskaya <dubrovskaya.darya104@gmail.com>
2024-04-01 09:14:10 +0300

    Correct mistakes

--- src/test/java/school/redrover/LoginAsHarryPotterGlobalsqaTest.java
+++ src/test/java/school/redrover/LoginAsHarryPotterGlobalsqaTest.java
@@ -10,31 +10,31 @@ import org.testng.annotations.Test;
 
 public class LoginAsHarryPotterGlobalsqaTest {
 
-        @Test
-        public void testLoginAsCustomerHarryPotterGlobalsqa() throws InterruptedException {
-            WebDriver driver = new ChromeDriver();
-            driver.get("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login");
+    @Test
+    public void testLoginAsCustomerHarryPotterGlobalsqa() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login");
 
-            Thread.sleep(3000);
+        Thread.sleep(1000);
 
-            WebElement customerLoginButton = driver.findElement(By.xpath("//button[@ng-click='customer()']"));
-            customerLoginButton.click();
+        WebElement customerLoginButton = driver.findElement(By.xpath("//button[@ng-click='customer()']"));
+        customerLoginButton.click();
 
-            Thread.sleep(3000);
+        Thread.sleep(1000);
 
-            Select select = new Select(driver.findElement(By.id("userSelect")));
-            select.selectByIndex(2);
+        Select select = new Select(driver.findElement(By.id("userSelect")));
+        select.selectByIndex(2);
 
-            WebElement loginButton = driver.findElement(By.xpath("//button[@type = 'submit']"));
-            loginButton.click();
+        WebElement loginButton = driver.findElement(By.xpath("//button[@type = 'submit']"));
+        loginButton.click();
 
-            Thread.sleep(3000);
+        Thread.sleep(1000);
 
-            WebElement welcomeText = driver.findElement(By.xpath(" //strong[text() = ' Welcome ']"));
-            String s = welcomeText.getText();
+        WebElement welcomeText = driver.findElement(By.xpath("//strong[text() = ' Welcome ']"));
+        String s = welcomeText.getText();
 
-            Assert.assertEquals(s, "Welcome Harry Potter !!");
+        Assert.assertEquals(s, "Welcome Harry Potter !!");
 
-            driver.quit();
-        }
+        driver.quit();
     }
+}
