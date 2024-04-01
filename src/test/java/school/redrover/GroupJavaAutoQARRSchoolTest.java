4dd3b5a2fbe585af15a73915a99c4647153e63f6
Natalia <130189899+vabaska@users.noreply.github.com>
2024-04-01 13:09:06 -0400

    add import (#419)

--- src/test/java/school/redrover/GroupJavaAutoQARRSchoolTest.java
+++ src/test/java/school/redrover/GroupJavaAutoQARRSchoolTest.java
@@ -1,9 +1,6 @@
 package school.redrover;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
