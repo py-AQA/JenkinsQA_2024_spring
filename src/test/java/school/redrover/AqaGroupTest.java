59bb9c4d5c2a6e83fef96755d35e84650f8e7eb8
Evgeniy <119495875+Gaztechica@users.noreply.github.com>
2024-04-02 11:29:18 +0400

    add-test2 (#454)
    * add-test2
    
    * add-test2
    
    * add-test2

--- src/test/java/school/redrover/AqaGroupTest.java
+++ src/test/java/school/redrover/AqaGroupTest.java
@@ -16,10 +16,7 @@ import org.testng.annotations.Test;
 import java.net.URISyntaxException;
 import java.nio.file.Paths;
 import java.time.Duration;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.concurrent.TimeUnit;
+import java.util.*;
 
 public class AqaGroupTest extends AqaGroupBaseTest {
 
@@ -39,18 +36,13 @@ public class AqaGroupTest extends AqaGroupBaseTest {
     private static final String INPUT_EMAIL = "//input[@class='ant-input primaryInput  not-entered']";
     private static final String BTN_PASSWORD = "//button[@class='ant-btn ant-btn-default authButton big colorPrimary ']";
     private static final By GET_ERROR = By.xpath("//div[@style='text-align: center; margin-bottom: 20px; color: rgb(255, 0, 0);']");
+    private static final By GET_POLITICA = By.xpath("//h1[@class='page-header-title clr']");
 
 
     private String calc(String x) {
         return String.valueOf(Math.log(Math.abs(12 * Math.sin(Integer.parseInt(x)))));
     }
 
-    private void url() {
-        getDriver().get(URL_MOB);
-        getDriver().manage().window().setSize(new Dimension(1920, 1080));
-        getDriver().manage().timeouts().pageLoadTimeout(25, TimeUnit.SECONDS);
-        getDriver().manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);
-    }
 
     @Test
     public void testAlert() {
@@ -937,7 +929,6 @@ public class AqaGroupTest extends AqaGroupBaseTest {
     }
 
     @Test
-
     public void testButtonChangeItsName() {
         getDriver().get("http://uitestingplayground.com/textinput");
 
@@ -967,13 +958,25 @@ public class AqaGroupTest extends AqaGroupBaseTest {
         Assert.assertFalse(getDriver().findElement(By.xpath(xpath)).isDisplayed(), "Not all the buttons are hidden!");
      }
 
-    public void testRemovesPasword() {
-        url();
+    @Test
+    public void testRemovesPassword() {
+        getDriver().get(URL_MOB);
         getDriver().findElement(By.xpath(INPUT_EMAIL)).sendKeys("yyyyyyyyyy@mail.xx");
         getDriver().findElement(By.xpath(BTN_PASSWORD)).click();
 
-        String getError = getDriver().findElement(GET_ERROR).getText();
+        Assert.assertEquals(getDriver().findElement(GET_ERROR).getText(), "Неправильный логин или пароль");
+    }
 
-        Assert.assertEquals(getError, "Неправильный логин или пароль");
+    @Test
+    public void testHrefPolitic() {
+        getDriver().get(URL_MOB);
+
+        getWait15().until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@href='https://vr-arsoft.com/personal-data-processing-policy/']"))).click();
+
+        Set<String> handles = getDriver().getWindowHandles();
+        handles.remove(getDriver().getWindowHandle());
+        getDriver().switchTo().window(handles.iterator().next());
+
+        Assert.assertEquals(getDriver().findElement(GET_POLITICA).getText(), "Политика обработки персональных данных");
     }
 }
