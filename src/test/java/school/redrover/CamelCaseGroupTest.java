413bf404d8359801e12c468803d85db36efb5e11
Diana <krukava.qamanual@gmail.com>
2024-04-02 23:25:56 +0300

    Add  new test

--- src/test/java/school/redrover/CamelCaseGroupTest.java
+++ src/test/java/school/redrover/CamelCaseGroupTest.java
@@ -1,6 +1,8 @@
 package school.redrover;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
@@ -19,5 +21,26 @@ public class CamelCaseGroupTest extends BaseTest {
 
         Assert.assertEquals(currentUrl, "https://www.fairfaxicearena.com/pro-shop.html");
         Assert.assertTrue(appointmentRequestForm.isDisplayed());
+    }
+    @Test
+    public void testTools() throws InterruptedException {
+
+        getDriver().get("https://www.saucedemo.com/");
+
+        WebElement text = getDriver().findElement(By.id("user-name"));
+        text.sendKeys("standard_user");
+
+        WebElement text1 = getDriver().findElement(By.id("password"));
+        text1.sendKeys("secret_sauce");
+
+        WebElement button = getDriver().findElement(By.id("login-button"));
+        button.click();
+
+        Thread.sleep(1000);
+
+        WebElement link = getDriver().findElement(By.className("app_logo"));
+        String resultText = link.getText();
+
+        Assert.assertEquals(resultText, "Swag Labs");
     }
 }
