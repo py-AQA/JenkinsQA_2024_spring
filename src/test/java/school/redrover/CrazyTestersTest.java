4ca01e796d1235a51fbc19ee0ab4e41e29e9e476
kas-113 <allakravtsova.113@gmail.com>
2024-04-02 14:59:01 +0200

    creating conflict

--- src/test/java/school/redrover/CrazyTestersTest.java
+++ src/test/java/school/redrover/CrazyTestersTest.java
@@ -92,4 +92,34 @@ public class CrazyTestersTest extends BaseTest {
         driver.quit();
 
     }
+
+    @Test
+    public void w3schoolTest() throws InterruptedException {
+
+        WebDriver driver = getDriver();
+        driver.get("https://www.w3schools.com/");
+
+        WebElement link = driver.findElement(By.linkText("Java Tutorial"));
+        link.click();
+
+        WebElement menu = driver.findElement(By.cssSelector("#menubtn_container > span > a"));
+        menu.click();
+
+        WebElement link2 = driver.findElement(By.linkText("Java Variables"));
+        link2.click();
+
+        WebElement text1 = driver.findElement(By.name("ex1"));
+        text1.sendKeys("String");
+
+        WebElement text2 = driver.findElement(By.name("ex2"));
+        text2.sendKeys("carName");
+
+        WebElement text3 = driver.findElement(By.name("ex3"));
+        text3.sendKeys("\"Volvo\"");
+
+        WebElement button = driver.findElement(By.cssSelector("#w3-exerciseform > div > button"));
+        String resultText = button.getText();
+        Assert.assertEquals(resultText, "Submit Answer »");
+        button.click();
+    }
 }
