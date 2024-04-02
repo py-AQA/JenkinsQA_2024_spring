1cf60c030ab17b5661f33347876fb161462d252c
Elena Pozhydaieva <epozhydaieva@gmail.com>
2024-04-02 01:21:25 -0400

    add corrections to my test

--- src/test/java/school/redrover/CamelCaseGroupTest.java
+++ src/test/java/school/redrover/CamelCaseGroupTest.java
@@ -5,23 +5,19 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
-import java.time.Duration;
-
 public class CamelCaseGroupTest extends BaseTest {
     @Test
-    public void checkRedirectionProShopTest() {
-        getDriver().manage().timeouts().implicitlyWait(Duration.ofMillis(500));
-
+    public void testCheckRedirectionProShop() {
         getDriver().get("https://www.fairfaxicearena.com/");
 
         WebElement proShopBtn = getDriver().findElement(By.xpath("//a[normalize-space()='Pro Shop']"));
         proShopBtn.click();
 
         String currentUrl = getDriver().getCurrentUrl();
-        Assert.assertEquals(currentUrl, "https://www.fairfaxicearena.com/pro-shop.html");
 
         WebElement appointmentRequestForm = getDriver().findElement(By.xpath("//span[contains(text(),'Pro Shop Appointment Request Form')]"));
 
+        Assert.assertEquals(currentUrl, "https://www.fairfaxicearena.com/pro-shop.html");
         Assert.assertTrue(appointmentRequestForm.isDisplayed());
     }
 }
