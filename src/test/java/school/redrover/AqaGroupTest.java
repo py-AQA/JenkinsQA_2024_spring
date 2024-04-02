54fbc8cee8b58b9085f5e82ce19c37f26b9edff7
aqaestuser <144237758+aqaestuser@users.noreply.github.com>
2024-04-02 23:18:16 +0300

    [+] set (#493)

--- src/test/java/school/redrover/AqaGroupTest.java
+++ src/test/java/school/redrover/AqaGroupTest.java
@@ -19,6 +19,7 @@ import java.time.Duration;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
 
 public class AqaGroupTest extends AqaGroupBaseTest {
 
