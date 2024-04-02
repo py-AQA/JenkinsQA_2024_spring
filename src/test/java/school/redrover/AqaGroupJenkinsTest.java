eb485553cd0b824122dba4e8fe863dede9ddd58f
iKuper <160205258+KuperWoman@users.noreply.github.com>
2024-04-02 15:42:36 +0300

    iKuper/jenkins test (#467)
    * add jenkins items tests

--- src/test/java/school/redrover/AqaGroupJenkinsTest.java
+++ src/test/java/school/redrover/AqaGroupJenkinsTest.java
@@ -1,6 +1,8 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -14,12 +16,38 @@ public class AqaGroupJenkinsTest extends AqaGroupBaseTest {
         getDriver().findElement(By.name("Submit")).click();
     }
 
+    private void createItem(String name, String itemClassName) {
+        getDriver().findElement(By.cssSelector("#side-panel > div > div")).click();
+        getWait15().until(ExpectedConditions.presenceOfElementLocated(By.id("name"))).sendKeys(name);
+        getDriver().findElement(By.className(itemClassName)).click();
+        getDriver().findElement(By.id("ok-button")).click();
+    }
+
+    private void deleteItem(String name) {
+        if (!getDriver().findElements(By.cssSelector(String.format("a[href = 'job/%s/']", name))).isEmpty()) {
+            new Actions(getDriver())
+                    .moveToElement(getDriver().findElement(
+                            By.cssSelector(String.format("a[href = 'job/%s/']", name))))
+                    .pause(1000)
+                    .moveToElement(getDriver().findElement(
+                            By.cssSelector(String.format("button[data-href = 'http://localhost:8080/job/%s/']", name))))
+                    .click()
+                    .perform();
+
+            getDriver().findElement(By.cssSelector(String.format("button[href='/job/%s/doDelete']", name))).click();
+            getDriver().findElement(By.className("jenkins-button--primary")).click();
+        }
+    }
+
+    private void returnToDashBoard() {
+        getDriver().findElement(By.cssSelector("a[href = '/']")).click();
+    }
+
     @Test
     public void testAuthJenkins() {
         login();
 
-        Assert.assertTrue(
-                getDriver().findElement(By.cssSelector("a[href = '/logout']")).isDisplayed());
+        Assert.assertTrue(getDriver().findElement(By.cssSelector("a[href = '/logout']")).isDisplayed());
     }
 
     @Test
@@ -28,24 +56,45 @@ public class AqaGroupJenkinsTest extends AqaGroupBaseTest {
 
         getDriver().findElement(By.cssSelector("a[href = '/logout']")).click();
 
-        Assert.assertEquals(
-                getDriver().findElement(By.tagName("h1")).getText(),
-                "Sign in to Jenkins");
+        Assert.assertEquals(getDriver().findElement(By.tagName("h1")).getText(),"Sign in to Jenkins");
     }
 
     @Test
     public void testNewItemJenkins() {
         login();
 
-        getDriver().findElement(By.cssSelector("#side-panel > div > div")).click();
-        getDriver().findElement(By.id("name")).sendKeys("my_number_one");
-        getDriver().findElement(By.className("hudson_model_FreeStyleProject")).click();
-        getDriver().findElement(By.id("ok-button")).click();
+        deleteItem("FP");
 
-        getDriver().findElement(By.cssSelector("a[href = '/']")).click();
+        createItem("FP", "hudson_model_FreeStyleProject");
 
-        Assert.assertTrue(
-                getDriver().findElement(By.cssSelector("a[href = 'job/my_number_one/']"))
-                        .isDisplayed());
+        returnToDashBoard();
+
+        Assert.assertTrue(getDriver().findElement(By.cssSelector("a[href = 'job/FP/']")).isDisplayed());
+    }
+
+    @Test
+    public void testCreateMultiConfigurationProject() {
+        login();
+
+        createItem("MCP", "hudson_matrix_MatrixProject");
+
+        returnToDashBoard();
+
+        Assert.assertTrue(getDriver().findElement(By.cssSelector("a[href = 'job/MCP/']")).isDisplayed());
+    }
+
+    @Test
+    public void testAddDescription() {
+        login();
+
+        deleteItem("MCP");
+
+        createItem("MCP", "hudson_matrix_MatrixProject");
+
+        getWait5().until(ExpectedConditions.presenceOfElementLocated(By.xpath("//textarea[@name = 'description']")));
+        getDriver().findElement(By.xpath("//textarea[@name = 'description']")).sendKeys("xxx");
+        getDriver().findElement(By.xpath("//button[@formnovalidate = 'formNoValidate']")).click();
+
+        Assert.assertTrue(getDriver().findElement(By.cssSelector("div#description")).getText().startsWith("xxx"));
     }
 }
