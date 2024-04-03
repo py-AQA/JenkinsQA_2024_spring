ae106c047f90eae4c2b3ee462930b2bd5aec7aa8
SavinovMA <savinov.norton@yandex.ru>
2024-04-04 00:29:10 +0300

    Adding testLoginInvalidUser And testRemoveItemToCart

--- src/test/java/school/redrover/GroupUnitedByJava8Test.java
+++ src/test/java/school/redrover/GroupUnitedByJava8Test.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
