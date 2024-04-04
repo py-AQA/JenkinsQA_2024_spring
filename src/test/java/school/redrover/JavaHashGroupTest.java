c0b43a71a61d3908322dccb7f364093471f75295
Alex_299 <sir.nevajn@yandex.ru>
2024-04-04 15:58:39 +0300

    the first test's title was changed and ine more test was added

--- src/test/java/school/redrover/JavaHashGroupTest.java
+++ src/test/java/school/redrover/JavaHashGroupTest.java
@@ -11,7 +11,7 @@ import java.time.Duration;
 public class JavaHashGroupTest extends BaseTest {
 
     @Test
-    public void mainPageTitleTest() {
+    public void testMainPageTitle() {
 
         final String expectedTitle = "Swag Labs";
 
@@ -30,6 +30,60 @@ public class JavaHashGroupTest extends BaseTest {
         String actualTitle = getDriver().findElement(By.xpath("//div[contains(text(),'Swag Labs')]")).getText();
 
         Assert.assertEquals(actualTitle,expectedTitle);
+    }
+
+    @Test
+    public void testBuyingBlackShirt() {
+
+        getDriver().manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+
+        getDriver().get("https://www.saucedemo.com");
+
+        WebElement loginField = getDriver().findElement(By.xpath("//input[@id='user-name']"));
+        WebElement passField = getDriver().findElement(By.xpath("//input[@id='password']"));
+        WebElement submitButton = getDriver().findElement(By.xpath("//input[@id='login-button']"));
+
+        loginField.sendKeys("standard_user");
+        passField.sendKeys("secret_sauce");
+        submitButton.click();
+
+        WebElement tshirtTitle = getDriver().findElement(By.xpath("//div[@class='inventory_item_name '][contains(text(),'Sauce Labs Bolt T-Shirt')]"));
+        tshirtTitle.click();
+
+        WebElement tshirtPageTitle = getDriver().findElement(By.xpath("//div[@class='inventory_details_name large_size'][contains(text(),'Sauce Labs Bolt T-Shirt')]"));
+        Assert.assertTrue(tshirtPageTitle.isDisplayed());
+
+        WebElement addToCartButton = getDriver().findElement(By.xpath("//button[contains(text(),'Add to cart')]"));
+        addToCartButton.click();
+
+        WebElement goToCartButton = getDriver().findElement(By.xpath("//a[@class='shopping_cart_link']"));
+        goToCartButton.click();
+
+        WebElement descriptionOfItemInCart = getDriver().findElement(By.xpath("//div[@class='inventory_item_desc']"));
+        Assert.assertTrue(descriptionOfItemInCart.isDisplayed());
+
+        WebElement checkoutButton = getDriver().findElement(By.xpath("//button[@id='checkout']"));
+        checkoutButton.click();
+
+        WebElement firstNameField = getDriver().findElement(By.xpath("//input[@placeholder='First Name']"));
+        WebElement lastNameField = getDriver().findElement(By.xpath("//input[@placeholder='Last Name']"));
+        WebElement postCodeField = getDriver().findElement(By.xpath("//input[@placeholder='Zip/Postal Code']"));
+
+        firstNameField.sendKeys("David");
+        lastNameField.sendKeys("Adams");
+        postCodeField.sendKeys("777777");
+
+        WebElement continueButton = getDriver().findElement(By.xpath("//input[@id='continue']"));
+        continueButton.click();
+
+        WebElement checkoutPageTitle = getDriver().findElement(By.xpath("//span[contains(text(),'Checkout: Overview')]"));
+        Assert.assertTrue(checkoutPageTitle.isDisplayed());
+
+        WebElement finishButton = getDriver().findElement(By.xpath("//button[@id='finish']"));
+        finishButton.click();
+
+        WebElement successOrderSign = getDriver().findElement(By.xpath("//h2[contains(text(),'Thank you for your order!')]"));
+        Assert.assertTrue(successOrderSign.isDisplayed());
     }
 
     private static final String BAD_USERNAME = "username@gmail.com";
