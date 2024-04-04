013029fe28ce63fc106df65879d487d4325d04a8
Halyna A <125617798+7108581@users.noreply.github.com>
2024-04-03 19:31:46 -0700

    Forward Group test refactor (#539)

--- src/test/java/school/redrover/GroupForwardTest.java
+++ src/test/java/school/redrover/GroupForwardTest.java
@@ -2,36 +2,62 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
 
-public class GroupForwardTest {
+import java.util.concurrent.TimeUnit;
+
+public class GroupForwardTest extends BaseTest {
 
     @Test
     public void testCostco() {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.costco.com/");
-        driver.findElement(By.xpath("//*[@id='search-field']")).sendKeys("apple");
-        driver.findElement(By.xpath("//*[@id='search-field']")).sendKeys(Keys.ENTER);
 
-        Assert.assertEquals(driver.findElement(By.xpath("//*[@id='crumbs_ul']/li[2]/span")).getText(), "Apple Products");
+        getDriver().get("https://www.costco.com/");
+        getDriver().findElement(By.xpath("//*[@id='search-field']")).sendKeys("apple");
+        getDriver().findElement(By.xpath("//*[@id='search-field']")).sendKeys(Keys.ENTER);
 
-        driver.quit();
+        Assert.assertEquals(getDriver().findElement(By.xpath("//*[@id='crumbs_ul']/li[2]/span")).getText(), "Apple Products");
     }
 
     @Test
-    public void testCostcoReturnToHomePage() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.costco.com/apple-brand-showcase.html");
-        driver.findElement(By.xpath("//img[@automation-id = 'headerCostcoLogo']")).click();
-        driver.findElement(By.xpath("//*[@id='search-field']")).sendKeys(Keys.ENTER);
+    public void testCostcoReturnToHomePage(){
 
-        Assert.assertEquals(driver.getCurrentUrl(), "https://www.costco.com/");
+        getDriver().get("https://www.costco.com/apple-brand-showcase.html");
+        getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        getDriver().findElement(By.xpath("//*[@id='crumbs_ul']/li[1]/a/span")).click();
 
-        driver.quit();
+        Assert.assertEquals(getDriver().getCurrentUrl(), "https://www.costco.com/");
+    }
+//вопрос по тесту
+    @Ignore  @Test
+    public void testdemoQAElements() {
 
+       getDriver().get("https://demoqa.com/");
+       getDriver().findElement(By.xpath("//h5[contains(text(), 'Elements')]")).click();
+       getDriver().findElement(By.xpath("//span[text() ='Text Box']")).click();
+       getDriver().findElement(By.id("userName")).sendKeys("Bob Forest");
+       getDriver().findElement(By.id("userEmail")).sendKeys("a@example.com");
+       getDriver().findElement(By.id("currentAddress")).sendKeys("8410 N. Rafael Rivera Way, Las Vegas, NV 89113");
+       getDriver().findElement(By.id("permanentAddress")).sendKeys("11011 W Charleston Blvd, Las Vegas, NV 89135");
+       getDriver().findElement(By.cssSelector("button#submit"));
+
+        Assert.assertTrue(getDriver().findElement(By.id("output")).isDisplayed());
+        Assert.assertEquals(getDriver().findElement(By.xpath("//p[@id='name']/text()['Bob Forest'][1]")),"Bob Forest");
+    }
+
+    @Test
+    public void testVenetianrearaurantsHappyHour() {
+
+        getDriver().get("https://www.venetianlasvegas.com/");
+        getDriver().findElement(By.xpath("//a[@data-navigation-title='Restaurants']")).click();
+        getDriver().findElement(By.xpath("//a [contains(text(), 'Happy')]")).click();
+        getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        String text = getDriver().findElement(By.xpath("//div[@class='mod-restaurant-browse-hero']//h1")).getText();
+
+        Assert.assertTrue(getDriver().findElement(By.xpath("//div[@class='mod-restaurant-browse-hero']")).isDisplayed());
+        Assert.assertEquals(text, "Happy Hour");
     }
 }
 
