0f81337d89ea9e12ebbbe2fbd8245b60f13d5eb9
Yuliya Piatrova <110020559+yuliyapiatrova@users.noreply.github.com>
2024-04-07 14:57:05 -0400

    add footer test (#586)

---
+++ src/test/java/school/redrover/FooterTest.java
@@ -0,0 +1,18 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+public class FooterTest extends BaseTest {
+
+    @Test
+    public void testJenkinsVersionFooter() {
+        WebElement jenkinsVersion = getDriver().findElement(By.xpath("//*[@class='jenkins-button jenkins-button--tertiary jenkins_ver']"));
+
+        Assert.assertEquals(jenkinsVersion.getText(), "Jenkins 2.440.2");
+    }
+
+}
