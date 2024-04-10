3f955b50af4ac7d382f7787ffa5e77f819102ca7
Albina-star <akhalim1@student.ccp.edu>
2024-04-09 20:50:49 -0400

    test testDashboardMenu() more changes added

--- src/test/java/school/redrover/DashboardMenuTest.java
+++ src/test/java/school/redrover/DashboardMenuTest.java
@@ -30,13 +30,13 @@ public class DashboardMenuTest extends BaseTest {
                 .size();
 
         List<WebElement> dashboardMenu = getDriver().findElements(By.xpath("//div[@id='tasks']/div"));
-        List<String> actualDashboardMenu = WebElementsToString(dashboardMenu);
+        List<String> actualDashboardMenu = webElementsToString(dashboardMenu);
 
         Assert.assertEquals(actualDashboardMenuSize, 5);
         Assert.assertEquals(actualDashboardMenu, expectedDashboardMenu );
     }
 
-    public static List<String> WebElementsToString(List<WebElement> dashboardMenu) {
+    private static List<String> webElementsToString(List<WebElement> dashboardMenu) {
         List<String> stringList = new ArrayList<>();
         for (WebElement element : dashboardMenu) {
             stringList.add(element.getText());
