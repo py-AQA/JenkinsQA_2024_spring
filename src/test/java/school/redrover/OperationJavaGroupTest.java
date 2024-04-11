82d84e899aa8b33674b97e962954e2584e05129e
Dorado <124732807+SvetlanaDorado@users.noreply.github.com>
2024-04-11 09:23:10 +0200

    Add class for group (#616)

---
+++ src/test/java/school/redrover/OperationJavaGroupTest.java
@@ -0,0 +1,19 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+public class OperationJavaGroupTest extends BaseTest {
+
+    @Test
+
+    public void testWelcome() {
+        WebElement welcomeText = getDriver().findElement(By.cssSelector(".empty-state-block > h1"));
+
+        Assert.assertEquals(welcomeText.getText(), "Welcome to Jenkins!");
+    }
+}
+
