2c853ab58f11b147680ccc0b3696fe494bb1ba84
Natalia <natalia.mylova@gmail.com>
2024-04-11 19:07:44 +0100

    delete one test

--- src/test/java/school/redrover/OperationJavaGroupTest.java
+++ src/test/java/school/redrover/OperationJavaGroupTest.java
@@ -8,13 +8,7 @@ import school.redrover.runner.BaseTest;
 
 public class OperationJavaGroupTest extends BaseTest {
 
-    @Test
 
-    public void testWelcome() {
-        WebElement welcomeText = getDriver().findElement(By.cssSelector(".empty-state-block > h1"));
-
-        Assert.assertEquals(welcomeText.getText(), "Welcome to Jenkins!");
-    }
     @Test
     public void testFreestyleProject() {
         getDriver().findElement(By.xpath("//a[.='New Item']")).click();
