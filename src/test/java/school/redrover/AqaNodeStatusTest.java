31bba09bad5fa7d2fcc7f0dbf21c7213a1e78991
iKuper <amalgammy@mail.ru>
2024-04-11 14:46:34 +0300

    add node status test

---
+++ src/test/java/school/redrover/AqaNodeStatusTest.java
@@ -0,0 +1,26 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AqaNodeStatusTest extends AqaBaseTest {
+
+    @Test
+    public void testNodeStatus() {
+//        Assert.assertTrue(getDriver().findElement(By.cssSelector("#executors th > span")).getText(). contains("offline"));
+        Assert.assertTrue(getDriver().findElement(By.id("executors")).getText().contains("Idle"));
+    }
+
+    @Test
+    public void testChangeNodeStatus() {
+        getDriver().findElement(By.cssSelector("[href = '/computer/']")).click();
+        getDriver().findElement(By.cssSelector("[data-href = '/computer/']")).click();
+        getDriver().findElement(By.className("jenkins-dropdown__item")).click();
+        getDriver().findElement(By.name("Submit")).click();
+        getWait15().until(ExpectedConditions.elementToBeClickable(getDriver().findElement(By.cssSelector("#main-panel button")))).click();
+
+        Assert.assertTrue(getDriver().findElement(By.className("message")).getText().contains("Disconnected"));
+    }
+}
