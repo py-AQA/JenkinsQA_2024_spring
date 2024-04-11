e847c0715b690fceb33e7a0a229fce7cc6289bd1
iKuper <amalgammy@mail.ru>
2024-04-11 14:46:51 +0300

    add node status test

--- src/test/java/school/redrover/AqaBreadcrumbDropdownMenuTest.java
+++ src/test/java/school/redrover/AqaBreadcrumbDropdownMenuTest.java
@@ -3,7 +3,6 @@ package school.redrover;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
