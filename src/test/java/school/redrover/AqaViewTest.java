71b09ef1d26da277e9a089a2f07c9270625fefab
iKuper <160205258+KuperWoman@users.noreply.github.com>
2024-04-12 12:48:40 +0300

    iKuper/view tests (#20)
    * add view test
    
    * add view tests

--- src/test/java/school/redrover/AqaViewTest.java
+++ src/test/java/school/redrover/AqaViewTest.java
@@ -9,8 +9,35 @@ public class AqaViewTest extends AqaBaseTest{
     @Test
     public void testCreateView() {
         createItemAndReturnToDashboard("Project", Item.FREESTYLE_PROJECT);
-        addView("Second", View.MY_VIEW);
+        createView("Second", View.MY_VIEW);
 
-        Assert.assertTrue(getDriver().findElement(By.cssSelector(".tab.active")).getText().contains("Second"));
+        Assert.assertTrue(getDriver().findElement(By.cssSelector(".tab.active")).getText().contains("Second"),
+                "The view has not been created");
+    }
+
+    @Test
+    public void testDeleteView() {
+        createItemAndReturnToDashboard("Folder", Item.FOLDER);
+        createView("I_see", View.MY_VIEW);
+
+        getDriver().findElement(By.cssSelector("[href = '/view/I_see/']")).click();
+        getDriver().findElement(By.className("icon-edit-delete")).click();
+        getDriver().findElement(By.className("jenkins-button--primary")).click();
+
+        Assert.assertTrue(getDriver().findElements(By.cssSelector("[href = '/view/I_see/']")).isEmpty(),
+                "The view has not been deleted");
+    }
+
+    @Test
+    public void testAddDescriptionToView() {
+        createItemAndReturnToDashboard("Folder", Item.FOLDER);
+        createView("I_see", View.MY_VIEW);
+
+        getDriver().findElement(By.cssSelector("[href = '/view/I_see/configure']")).click();
+        getDriver().findElement(By.name("_.description")).sendKeys("I love Jenkins");
+        getDriver().findElement(By.name("Submit")).click();
+
+        Assert.assertTrue(getDriver().findElement(By.id("description")).getText().contains("I love Jenkins"),
+                "No description has been added");
     }
 }
