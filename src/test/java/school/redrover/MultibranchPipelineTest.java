f1e623e9e2b63b677d54642b2b234c94aeffdb94
Pavel Pugach <PPugach@qulix.com>
2024-04-13 05:30:54 +0300

    TC_05.003.01: решение конфликта

--- src/test/java/school/redrover/MultibranchPipelineTest.java
+++ src/test/java/school/redrover/MultibranchPipelineTest.java
@@ -3,7 +3,6 @@ package school.redrover;
 import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
